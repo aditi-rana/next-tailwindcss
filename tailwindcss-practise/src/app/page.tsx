7935b734d96d2e452d65ae82606fd0920cd29da3
Aditi Rana <aditirana299@gmail.com>
2024-11-04 23:28:58 +0530

    grid added

--- tailwindcss-practise/src/app/page.tsx
+++ tailwindcss-practise/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Navbar from "@/app/components/Navbar";
 import Sidebar from "@/app/components/Sidebar";
+import CardGroup from "@/app/components/Cards";
 export default function Home() {
   return (
     <>
@@ -9,20 +10,22 @@ export default function Home() {
         <Sidebar />
         <div className="w-full">
           <Navbar />
-          <div className="min-h-screen bg-black">
+          <main className="min-h-screen bg-black">
             <div className="bg-emerald-950 flex items-center justify-between space-y-2 flex-col md:flex-row rounded mt-1 mr-1 p-4">
               <h1 className="font-bold text-green-500">
                 Welcome to Tailwind CSS
               </h1>
               <button
-                className="bg-lime-300 p-2 rounded-lg"
+                className="bg-lime-300 p-2 rounded-lg text-black"
                 onClick={() => alert("yoohoooo!")}
               >
                 Click Me
               </button>
             </div>
-            <div className="text-color">HIIIII</div>
-          </div>
+            <div className="flex justify-center">
+              <CardGroup />
+            </div>
+          </main>
         </div>
       </div>
     </>
